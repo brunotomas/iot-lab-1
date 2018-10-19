539d1c17254e728e13abf9096ed7812bc2ec4b8f
Francisco Romulo Freires do Nascimento <romulofreires1@gmail.com>
2018-10-19 18:46:06 -0300

    versão mvc 2

--- led-client/src/utils/BoundData.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public final class BoundData {
-
-	public static double minTemp = 26.0;
-	public static double maxTemp = 26.0;
-	public static double minHumidity = 86.0;
-	public static double maxHumidity = 86.0;
-}
